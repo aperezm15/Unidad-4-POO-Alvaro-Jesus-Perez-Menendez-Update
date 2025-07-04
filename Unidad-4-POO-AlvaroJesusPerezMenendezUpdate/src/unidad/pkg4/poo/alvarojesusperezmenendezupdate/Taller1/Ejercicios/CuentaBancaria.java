991007ff7c73850859bb7fa98bae453bfc209d63
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-03 23:42:43 -0500

    feat: Alvaro Jesus Perez Menendez - Taller1 X Ejercicio2

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller1/Ejercicios/CuentaBancaria.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller1.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class CuentaBancaria {
+    int numeroCuenta;
+    double saldo;
+    String tipoCuenta;
+
+    public CuentaBancaria() {
+        numeroCuenta = 0;
+        saldo = 0;
+        tipoCuenta = "Desconocido";
+        
+    }
+
+    public CuentaBancaria(int numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    public CuentaBancaria(int numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+    
+    
+
+    
+    
+    
+}
--- Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller1/Ejercicios/Libro.java
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller1/Ejercicios/Libro.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller1.Ejercicio1;
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller1.Ejercicios;
 
 /**
  *
