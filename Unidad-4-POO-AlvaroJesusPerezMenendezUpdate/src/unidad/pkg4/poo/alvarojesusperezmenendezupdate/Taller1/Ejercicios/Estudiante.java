a8c450d765fb4ddf26c3fd6aa5554b07d69d77d0
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-04 00:03:17 -0500

    feat: Alvaro Jesus Perez Menendez - Taller1 X Ejercicio3

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller1/Ejercicios/Estudiante.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller1.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Estudiante {
+    String nombre;
+    int edad;
+    String curso;
+
+    public Estudiante() {
+        nombre = "Desconocido";
+        edad = 0;
+        curso = "Desconocido";
+    }
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+
+    public Estudiante(String nombre, int edad, String curso) {
+        this(nombre);
+        this.edad = edad;
+        this.curso = curso;
+        
+    }
+    
+    
+}
+    
+    
+    
+    
+    
+   
