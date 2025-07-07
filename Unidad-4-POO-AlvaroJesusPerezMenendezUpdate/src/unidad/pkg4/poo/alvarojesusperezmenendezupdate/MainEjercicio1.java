81d74623b7483876c7e63e982ab4f9b5fb08d4a6
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-07 07:51:01 -0500

    feat: Alvaro Jesus Perez Menendez - Taller12 X Ejercicio 1

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/MainEjercicio1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate;
+
+import unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios.Ave;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class MainEjercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ave ave1 = new Ave();
+        ave1.cantar();
+        ave1.volar();
+    }
+    
+}
---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller12/Ejercicios/Ave.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Ave implements Cantante,Volador {
+
+    @Override
+    public void cantar() {
+        System.out.println("El ave puede cantar");
+    }
+
+    @Override
+    public void volar() {
+        System.out.println("El ave puede volar");
+    }
+    
+
+}
