1d24b8be9f8b7ce00ed497f8fd16af45cc7d0d43
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-07 08:45:21 -0500

    feat: Alvaro Jesus Perez Menendez - Taller12 X Ejercicio 5

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/MainEjercicio5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate;
+
+import unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios.Animal;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class MainEjercicio5 {
+    
+    public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        animal1.nadar();
+        animal1.volar();
+    }
+}
---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller12/Ejercicios/Animal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class Animal implements Nadador,Volador {
+
+    @Override
+    public void nadar() {
+        System.out.println("El animal esta nadando");
+    }
+
+    @Override
+    public void volar() {
+        System.out.println("El animal esta volando");
+    }
+    
+}
