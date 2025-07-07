4e2ec1ea0a592cba1291457c7869aaecb3ee97a8
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-07 06:57:59 -0500

    feat: Alvaro Jesus Perez Menendez - Taller11 X Ejercicio 3

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/MainEjercicio3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate;
+
+import unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller11.Ejercicios.ClaseAbstracta;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class MainEjercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Nos dira que la clase no puede ser instanciada ya que es abstracta.
+        ClaseAbstracta prueba = new ClaseAbstracta();
+    }
+    
+}
---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller11/Ejercicios/ClaseAbstracta.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller11.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public abstract class ClaseAbstracta {
+    protected String atributo1;
+    
+    
+    //Dado que el metodo es abstracto, este no puede llevar un cuerpo..
+    //esto nos indica que no podemos implementar algo que se pueda sobreescribir
+    //en sus clases derivadas.
+    public abstract String metodo(){
+    
+    }
+    
+}
