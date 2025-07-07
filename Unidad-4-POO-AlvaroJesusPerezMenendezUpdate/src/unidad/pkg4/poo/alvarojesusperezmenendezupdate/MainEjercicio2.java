bd32a98b5a0accd04004097049f9ebdd9b8dbcd1
Alvaro Perez <aperezm15@unicartagena.edu.co>
2025-07-07 08:01:22 -0500

    feat: Alvaro Jesus Perez Menendez - Taller12 X Ejercicio 2

---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/MainEjercicio2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate;
+
+import unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios.Pez;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public class MainEjercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pez pez1 = new Pez();
+        pez1.nadar();
+        pez1.respirar();
+    }
+    
+}
---
+++ Unidad-4-POO-AlvaroJesusPerezMenendezUpdate/src/unidad/pkg4/poo/alvarojesusperezmenendezupdate/Taller12/Ejercicios/Nadador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.alvarojesusperezmenendezupdate.Taller12.Ejercicios;
+
+/**
+ *
+ * @author Mitzuke
+ */
+public interface Nadador {
+    void nadar();
+}
